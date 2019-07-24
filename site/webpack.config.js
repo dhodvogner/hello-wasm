b5dadd48cf19bc0c3db07244645edaf39690c43e
Daniel Hodvogner <dhodvogner@gmail.com>
2019-07-24 07:04:41 +0200

    WIP: wasm-pack-plugin added

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+// const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
 module.exports = {
+  mode: "development",
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
   },
-  mode: "development"
+  plugins: [
+    // new WasmPackPlugin({
+    //   crateDirectory: path.resolve(__dirname, "../"),
+    //   outName: "hello_wasm",
+    //   watchDirectories: [
+    //     path.resolve(__dirname, "../src")
+    //   ]
+    // }),
+  ]
 };
